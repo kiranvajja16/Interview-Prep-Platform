14bf70c857570c58d4913d7eb9d3f351e4f1c052
kiranvajja16 <vajjakiran2005@gmail.com>
2026-06-27 08:54:38 +0530

    Updated Production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api=axios.create({
-    baseURL:'https://interview-prep-backend-34tz.onrender.com/',
+    baseURL:'https://interview-prep-backend-34tz.onrender.com/api',
     headers:{
         'Content-Type':'application/json',
     },
